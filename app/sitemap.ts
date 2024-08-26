3ac5366822b897c2580254f72a91edd5bd1d3261
Stefan Straub <96797960+h0rus89@users.noreply.github.com>
2024-08-26 22:24:41 +0200

    sitemap.ts aktualisieren

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,10 @@ import { getBlogPosts } from 'app/db/blog';
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
     url: `https://leerob.io/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+    lastModified: post.metadata.date,
   }));
 
-  let routes = ['', '/blog', '/guestbook', '/uses', '/work'].map((route) => ({
+  let routes = ['', '/blog'].map((route) => ({
     url: `https://leerob.io${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
