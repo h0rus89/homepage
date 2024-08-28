2f5957cf1cb7fea44a356a39655f1d071858543c
Stefan Straub <zeolith_knorpelig_02@icloud.com>
2024-08-28 20:51:28 +0200

    update

--- components/upcoming.tsx
+++ components/upcoming.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Calendar } from "@/components/ui/calendar"
 import { de } from "date-fns/locale"
 
