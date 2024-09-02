3f35212492d2f8527b991e089fe25c47cea3c898
Stefan Straub <zeolith_knorpelig_02@icloud.com>
2024-09-02 22:38:09 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { FlipWords } from "@/components/ac/flip-words";
 
 export default function Page() {
+  const words = ["gemeinsam", "aktiv", "stark"];
+
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">Startseite</h1>
@@ -10,7 +13,14 @@ export default function Page() {
       <p className="prose prose-neutral dark:prose-invert">
         hier gehts zum <Link href="/cal">Kalender</Link>
       </p>
-      
+      <div className="mt-24 flex flex-col justify-center items-center px-4">
+      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+        Graf-Anton-Schule <br />
+      </div>
+      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+        <FlipWords words={words} /> 
+      </div>
+    </div>
     </section>
   );
 }
