c5443a7754b4079c1a6d25a8cba41fbf8e2c5c0a
Stefan Straub <zeolith_knorpelig_02@icloud.com>
2024-09-05 16:05:12 +0200

    loading

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+export default function Loading() {
+    return (
+      <section>
+        <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+          Seite wird geladen
+        </h1>
+        <p>
+          gleich geht es weiter ...
+        </p>
+      </section>
+    );
+  }
+  
